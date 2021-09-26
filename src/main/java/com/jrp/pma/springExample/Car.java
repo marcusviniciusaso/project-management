5159ffed94c0a9955063f08bf2ad1dea1ff77cf4
marcusviniciusaso <marcusvinicius_aso@yahoo.com.br>
2021-09-25 21:55:53 -0300

    first commit

---
+++ src/main/java/com/jrp/pma/springExample/Car.java
@@ -0,0 +1,15 @@
+package com.jrp.pma.springExample;
+
+public class Car {
+
+	Engine engine;
+	Doors doors;
+	Tires tires;
+	
+	public Car(Engine engine, Doors doors, Tires tires) {
+		super();
+		this.engine = engine;
+		this.doors = doors;
+		this.tires = tires;
+	}	
+}
---
+++ src/main/java/com/jrp/utils/DataCleansingUtil.java
@@ -0,0 +1,11 @@
+package com.jrp.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataCleansingUtil {
+
+	public DataCleansingUtil() {
+		super();
+	}	
+}
